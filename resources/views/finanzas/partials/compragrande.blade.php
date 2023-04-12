abeb50ed9d2237be57aa763471d8b3b0f62b4344
Sabiin <sabiintg@gmail.com>
2023-04-12 07:49:54 +0200

    FIX - UI fixes

--- resources/views/finanzas/partials/compragrande.blade.php
+++ resources/views/finanzas/partials/compragrande.blade.php
@@ -1,7 +1,7 @@
 <div class="compra-grande">
     <img src="../img/comprasgrandes/{{ $compragrande['id'] }}.png" alt="">
-    <p><span class="titulo">Fecha: </span> {{ $compragrande['fecha'] }} </p>
-    <p><span class="titulo">Nombre: </span> {{ $compragrande['nombre'] }} </p>
+    <p><span class="titulo">{{ $compragrande['fecha'] }}</span></p>
+    <p><span class="titulo">{{ $compragrande['nombre'] }}</span></p>
     <br>
     <p><span class="titulo">Total: </span> {{ $compragrande['total'] }}€<span class="titulo"> - €/mes:</span> {{ $compragrande['precio_mes'] }}€</p>
     <div class="botones">
