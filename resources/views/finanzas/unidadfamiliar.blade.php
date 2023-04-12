9c491454f010361f449ef29c90e8b5005ad6f22a
Sabiin <sabiintg@gmail.com>
2023-04-12 08:09:37 +0200

    FEAT - UnidadFamiliar now autogenerated

--- resources/views/finanzas/unidadfamiliar.blade.php
+++ resources/views/finanzas/unidadfamiliar.blade.php
@@ -8,27 +8,40 @@
         <h1>Unidad Familiar</h1>
     </div>
     <div class="main-content">
-        <div id="unidad-familiar">
-            <h2>¡No perteneces a una unidad familiar!</h2>
-            <div class="botones">
-                <button class="modificar">Unirse</button>
-                <button class="añadir">Crear</button>
-            </div>
-            <h2>Código: XXXXXX</h2>
-            <div></div>
-            <br>
-            <div id="integrantes">
-                <span><img src="../img/account_placeholder.png" alt=""></span>
-                <span><img src="../img/account_placeholder.png" alt=""></span>
-                <span><img src="../img/account_placeholder.png" alt=""></span>
-                <span><img src="../img/account_placeholder.png" alt=""></span>
-                <span><img src="../img/account_placeholder.png" alt=""></span>
-            </div>
-            <p><span class="titulo">Ingresos: </span>1000€</p>
-            <p><span class="titulo">Gastos: </span>500€</p>
 
-            <button class="borrar">Salir</button>
+        @php
+            $unidadfamiliar = [
+                'codigo' => '123456',
+                'integrantes' => ['integrante1', 'integrante2', 'integrante3', 'integrante4', 'integrante5'],
+                'ingresos' => '1000',
+                'gastos' => '500',
+            ];
+        @endphp
+        <div id="unidad-familiar">
+
+            @if (!isset($unidadfamiliar))
+                <h2>¡No perteneces a una unidad familiar!</h2>
+                <div class="botones">
+                    <button class="modificar">Unirse</button>
+                    <button class="añadir">Crear</button>
+                </div>
+            @else
+                <h2>Código: {{ $unidadfamiliar['codigo'] }}</h2>
+                <div></div>
+                <br>
+                <div id="integrantes">
+                    @foreach ($unidadfamiliar['integrantes'] as $integrante)
+                        <span><img src="../img/account_placeholder.png" alt=""></span>
+                    @endforeach
+                </div>
+                <br>
+                <p><span class="titulo">Ingresos: </span>1000€</p>
+                <p><span class="titulo">Gastos: </span>500€</p>
+                <div class="botones">
+                    <button class="borrar">Salir</button>
+                </div>
+            @endif
         </div>
     </div>
 
-@endsection
+    @endsection
