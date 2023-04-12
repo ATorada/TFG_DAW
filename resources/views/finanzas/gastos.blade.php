20f3b042ac7da246220fa5b592ef3ab3501296a9
Sabiin <sabiintg@gmail.com>
2023-04-12 07:58:31 +0200

    FEAT - Gastos now generated with foreach

--- resources/views/finanzas/gastos.blade.php
+++ resources/views/finanzas/gastos.blade.php
@@ -26,106 +26,41 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <select name="tipo" id="tipo">
-                                <option value="1">Alimentación</option>
-                                <option value="2">Vivienda</option>
-                                <option value="3">Transporte</option>
-                                <option value="4">Comunicaciones</option>
-                                <option value="5">Ocio</option>
-                                <option value="6">Salud</option>
-                                <option value="7">Educación</option>
-                                <option value="8">Otros</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <select name="tipo" id="tipo">
-                                <option value="1">Alimentación</option>
-                                <option value="2">Vivienda</option>
-                                <option value="3">Transporte</option>
-                                <option value="4">Comunicaciones</option>
-                                <option value="5">Ocio</option>
-                                <option value="6">Salud</option>
-                                <option value="7">Educación</option>
-                                <option value="8">Otros</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <select name="tipo" id="tipo">
-                                <option value="1">Alimentación</option>
-                                <option value="2">Vivienda</option>
-                                <option value="3">Transporte</option>
-                                <option value="4">Comunicaciones</option>
-                                <option value="5">Ocio</option>
-                                <option value="6">Salud</option>
-                                <option value="7">Educación</option>
-                                <option value="8">Otros</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <select name="tipo" id="tipo">
-                                <option value="1">Alimentación</option>
-                                <option value="2">Vivienda</option>
-                                <option value="3">Transporte</option>
-                                <option value="4">Comunicaciones</option>
-                                <option value="5">Ocio</option>
-                                <option value="6">Salud</option>
-                                <option value="7">Educación</option>
-                                <option value="8">Otros</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <select name="tipo" id="tipo">
-                                <option value="1">Alimentación</option>
-                                <option value="2">Vivienda</option>
-                                <option value="3">Transporte</option>
-                                <option value="4">Comunicaciones</option>
-                                <option value="5">Ocio</option>
-                                <option value="6">Salud</option>
-                                <option value="7">Educación</option>
-                                <option value="8">Otros</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
+                    @php
+                        $gastos = [
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'alimentacion', 'recurrente' => true],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'vivienda', 'recurrente' => false],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'transporte', 'recurrente' => true],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'comunicaciones', 'recurrente' => true],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'ocio', 'recurrente' => true],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'salud', 'recurrente' => false],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'educacion', 'recurrente' => true],
+                            ['fecha' => '01/01/2021', 'concepto' => 'Salario', 'cantidad' => '1000€', 'tipo' => 'otros', 'recurrente' => true],
+                        ];
+                    @endphp
+
+                    @foreach ($gastos as $gasto)
+                        <tr>
+                            <td>{{ $gasto['fecha'] }}</td>
+                            <td>{{ $gasto['concepto'] }}</td>
+                            <td>{{ $gasto['cantidad'] }}</td>
+                            <td>
+                                <select name="tipo" id="tipo">
+                                    <option value="alimentacion" {{ $gasto['tipo'] == 'alimentacion' ? 'selected' : '' }}>Alimentación</option>
+                                    <option value="vivienda" {{ $gasto['tipo'] == 'vivienda' ? 'selected' : '' }}>Vivienda</option>
+                                    <option value="transporte" {{ $gasto['tipo'] == 'transporte' ? 'selected' : '' }}>Transporte</option>
+                                    <option value="comunicaciones" {{ $gasto['tipo'] == 'comunicaciones' ? 'selected' : '' }}>Comunicaciones</option>
+                                    <option value="ocio" {{ $gasto['tipo'] == 'ocio' ? 'selected' : '' }}>Ocio</option>
+                                    <option value="salud" {{ $gasto['tipo'] == 'salud' ? 'selected' : '' }}>Salud</option>
+                                    <option value="educacion" {{ $gasto['tipo'] == 'educacion' ? 'selected' : '' }}>Educación</option>
+                                    <option value="otros" {{ $gasto['tipo'] == 'otros' ? 'selected' : '' }}>Otros</option>
+                                </select>
+                            <td>
+                                <input class="checkbox" type="checkbox" {{ $gasto['recurrente'] ? 'checked' : '' }}>
+                            </td>
+                        </tr>
+                    @endforeach
+
                 </tbody>
             </table>
         </div>
