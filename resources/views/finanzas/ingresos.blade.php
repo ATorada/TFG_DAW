fe1016b13f111d38d96a8c6739ab62186de479f9
Sabiin <sabiintg@gmail.com>
2023-04-12 08:03:02 +0200

    FEAT - Ingresos now generated with foreach

--- resources/views/finanzas/ingresos.blade.php
+++ resources/views/finanzas/ingresos.blade.php
@@ -21,46 +21,26 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>01/01/2021</td>
-                        <td>Salario</td>
-                        <td>1000€</td>
-                        <td>
-                            <input class="checkbox" type="checkbox">
-                        </td>
-                    </tr>
+                    @php
+                    $ingresos = [
+                        ['fecha' => '01/01/2021','concepto' => 'Salario','cantidad' => '1000€','recurrente' => true],
+                        ['fecha' => '01/01/2021','concepto' => 'Salario','cantidad' => '1000€','recurrente' => false],
+                        ['fecha' => '01/01/2021','concepto' => 'Salario','cantidad' => '1000€','recurrente' => true],
+                        ['fecha' => '01/01/2021','concepto' => 'Salario','cantidad' => '1000€','recurrente' => false],
+                        ['fecha' => '01/01/2021','concepto' => 'Salario','cantidad' => '1000€','recurrente' => true],
+                    ];
+                    @endphp
+
+                    @foreach ($ingresos as $ingreso)
+                        <tr>
+                            <td>{{ $ingreso['fecha'] }}</td>
+                            <td>{{ $ingreso['concepto'] }}</td>
+                            <td>{{ $ingreso['cantidad'] }}</td>
+                            <td>
+                                <input class="checkbox" type="checkbox" {{ $ingreso['recurrente'] ? 'checked' : '' }}>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
