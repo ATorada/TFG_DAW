35e6d7b40bf89b51a9a65c91ba836a193b1cec6e
Sabiin <sabiintg@gmail.com>
2023-04-18 07:50:35 +0200

    FEAT - 500 error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', '404')
+
+@section('content')
+
+    <div id="error">
+        <h1>500</h1>
+        <h2>Algo ha ocurrido con el servidor</h2>
+        <p>Por favor, sea paciente</p>
+    </div>
+
+@endsection
